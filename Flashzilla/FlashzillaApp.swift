9fc31d7a34c0f73a0a0a02d54e680e4a95ad7344
abgbro <codepath@abhigarlapati.com>
2024-05-06 21:03:20 -0400

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by user256510 on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
